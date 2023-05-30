769981ea87b687b3bd8c4dcd2a69813718c6f629
Rahulbbbb <beingbankoti@gmail.com>
2023-05-30 15:50:39 +0530

    third commit

--- src/component/FavMovieSlice.js
+++ src/component/FavMovieSlice.js
@@ -1,21 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const FavMovieSlice = createSlice({
-    name:"favMovie",
-    initialState:{
-        favMovie:[]
+    name: "favMovie",
+    initialState: {
+      favMovie: [],
     },
-    reducers:{
-        addMovie: (state, action)=>{
-            // state.favMovie = [...state.favMovie, action.payload];
-            // console.log("stateeeeee",state);
-            state.favMovie = state.favMovie.push(action.payload);
-        },
-        deleteMovie: (state, action)=>{
-            state.favMovie=state.favMovie.filter((movies)=>movies.imdbID !== action.payload)
-        }
-    }
-})
-
-export const { addMovie, deleteMovie } = FavMovieSlice.actions;
-export const FavMovieReducer = FavMovieSlice.reducer;
+    reducers: {
+      addMovie: (state, action) => {
+        state.favMovie.push(action.payload);
+      },
+      deleteMovie: (state, action) => {
+        state.favMovie = state.favMovie.filter((movie) => movie.imdbID !== action.payload);
+      },
+    },
+  });
+  
+  export const { addMovie, deleteMovie } = FavMovieSlice.actions;
+  export const FavMovieReducer = FavMovieSlice.reducer;
+  
--- src/component/Favourites.js
+++ src/component/Favourites.js
@@ -1,15 +1,41 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import styles from './Favourites.module.css';
+import { deleteMovie } from './FavMovieSlice';
 
 function Favourites() {
-    const favMovieState = useSelector((state)=>state.favMovie);
+  const favMovieState = useSelector((state) => state.favMovie);
+  const dispatch = useDispatch();
+
+  const handleDeleteMovie = (movieId) => {
+    dispatch(deleteMovie(movieId));
+  };
+
   return (
-    <div>Favourites
-        {
-            favMovieState.favMovie.map((Movies)=> <div>{Movies.Title}</div>)
-        }
-    </div>
-  )
+    <>
+      <h1>Favourites</h1>
+      <div className={styles.movieContainer}>
+        {Array.isArray(favMovieState.favMovie) ? (
+          favMovieState.favMovie.map((movie) => (
+            <div className={styles.movieCard} key={movie.imdbID}>
+              <img className={styles.image} src={movie.Poster} alt="poster" />
+              <h3 className={styles.name}>{movie.Title}</h3>
+              <span>({movie.Year})</span>
+              <br />
+              <button
+                className={styles.button}
+                onClick={() => handleDeleteMovie(movie.imdbID)}
+              >
+                Delete
+              </button>
+            </div>
+          ))
+        ) : (
+          <div>No favorite movies</div>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default Favourites;
